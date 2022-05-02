c94014f06523311c60d084fa81eafe6d0b76fa02
Adrial-Knight <pierreminier64@gmail.com>
2022-05-03 00:20:50 +0200

    [Clavier virtuel] ajouté

--- scripts/tusmo.js
+++ scripts/tusmo.js
@@ -1,58 +1,52 @@
 import {Line} from "./Line.js"
 import {Game} from "./Game.js"
 
-var game = undefined
-
-await fetch("data/dico.json")
+var game = await fetch("data/dico.json")
         .then(response => response.json())
         .then(dico => game = new Game(dico))
+var line = undefined
 
-var line = game.init(5, 6)
-tusmoKeyboardHandler()
-
-function tusmoKeyboardHandler(){
+function handler(key, game) {
+    console.log(key)
     var status = undefined
-    const hint = line.getHintList()
-    var handler
-    document.addEventListener("keydown", handler = function(event) {
-        if (event.key === "Enter" && game.isRunning()) {
-            if (game.allow(line.getCache())){
-                status = game.analyse(line.getCache())
-                line.refresh(status)
-                if (allAreEqual(status)){
-                    document.removeEventListener("keydown", handler)
-                    game.win()
-                }
-                else{
-                    game.useAChance()
-                    if(game.isRunning()){
-                        line = new Line(line.getFirstLetter(), line.getLenght())
-                        line.refresh([])
-                    }
+    if (key === "ENTER" && game.isRunning()) {
+        if (game.allow(line.getCache())){
+            var status = game.analyse(line.getCache())
+            line.refresh(status)
+            if (allAreEqual(status)){
+                game.win()
+            }
+            else{
+                game.useAChance()
+                if(game.isRunning()){
+                    line = new Line(line.getFirstLetter(), line.getLenght())
+                    line.refresh([])
                 }
             }
-            else {
-                line.deleteCache()
-            }
         }
-        else if (event.key === "Delete" || event.key === "Backspace"){
-            line.pop()
-        }
-        else if (isLetter(event.key)){
-            line.push(event.key)
-        }
-        if (!allAreEqual(status) && !game.isRunning()){
-            document.removeEventListener("keydown", handler)
-            game.over()
-        }
-        else if (game.isRunning()){
-            const analyse = Array(line.getCache().length).fill(hint.ingame)
-            line.refresh(analyse)
+        else {
+            line.deleteCache()
         }
     }
-)}
+    else if (key === "DELETE" || key === "BACKSPACE"){
+        line.pop()
+    }
+    else if (isLetter(key)){
+        line.push(key)
+    }
+    if (!allAreEqual(status) && !game.isRunning()){
+        game.over()
+    }
+    else if (game.isRunning()){
+        const hint = line.getHintList()
+        const analyse = Array(line.getCache().length).fill(hint.ingame)
+        line.refresh(analyse)
+    }
+}
 
-const isLetter = str => (/^[a-zA-Z]*$/.test(str) && str.length == 1);
+function isLetter(str){
+    return /^[a-zA-Z]*$/.test(str) && str.length == 1
+}
 
 function allAreEqual(array) {
     if(!array){
@@ -65,3 +59,16 @@ function allAreEqual(array) {
     });
     return result;
 }
+
+
+line = game.init(6, 6)
+console.log(game)
+
+document.addEventListener("keydown", function(event){
+    if (game.isRunning())
+        handler(event.key.toUpperCase(), game)
+})
+
+document.getElementById("virtual-keyboard").addEventListener("click", function(event){
+    handler(event.target.id, game)
+})
